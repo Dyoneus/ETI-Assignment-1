5ac221f905ed2796856380bee5c84b2ac35b840d
Dyoneus <jiayuan999@hotmail.com>
2023-11-21 12:58:19 +0800

    Add: Database initialization and connection closing

--- user-service/main.go
+++ user-service/main.go
@@ -6,9 +6,25 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// Initialize the database connection.
+	dsn := "username:password@tcp(127.0.0.1:5000)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to the database: %v", err)
+	}
+
+	// Make sure to defer the closing of the database connection.
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Could not get database: %v", err)
+	}
+	defer sqlDB.Close()
+
 	// Set up the router.
 	r := mux.NewRouter()
 
