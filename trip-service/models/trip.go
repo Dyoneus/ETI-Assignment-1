3393762a710358acb9bf29ee0dd6cd209240d0b3
Dyoneus <jiayuan999@hotmail.com>
2023-11-21 13:49:36 +0800

    Add: Trip model to trip-service

---
+++ trip-service/models/trip.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// Trip represents a car-pooling trip published by a car owner.
+type Trip struct {
+	gorm.Model
+	CarOwnerID         uint      `json:"car_owner_id"`
+	PickUpLocation     string    `json:"pick_up_location"`
+	AlternativePickUp  string    `json:"alternative_pick_up"`
+	TravelStartTime    time.Time `json:"travel_start_time"`
+	DestinationAddress string    `json:"destination_address"`
+	AvailableSeats     int       `json:"available_seats"`
+	EnrolledPassengers int       `json:"enrolled_passengers"`
+}
