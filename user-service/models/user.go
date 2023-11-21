afa288ba574754ebe0987c6c20db6e661ca58ac6
Dyoneus <jiayuan999@hotmail.com>
2023-11-21 13:45:15 +0800

    Add: Create two model profile

---
+++ user-service/models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+// User represents a user in the system with a default passenger profile.
+type User struct {
+	gorm.Model
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Mobile    string `json:"mobile"`
+	Email     string `gorm:"uniqueIndex" json:"email"`
+}
+
+// CarOwnerProfile represents a car owner's profile, extending the User model.
+type CarOwnerProfile struct {
+	UserID         uint   `gorm:"primaryKey"`
+	DriversLicense string `json:"drivers_license"`
+	CarPlateNumber string `json:"car_plate_number"`
+}
