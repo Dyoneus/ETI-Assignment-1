8e91156f9c70e19df335405cc82340e6ad3d5ac2
Dyoneus <jiayuan999@hotmail.com>
2023-11-21 13:42:53 +0800

    Init: trip-service main.go file

--- trip-service/main.go
+++ trip-service/main.go
@@ -1,3 +1,22 @@
 // By Ong Jia Yuan S10227735B
 // main.go in trip-service directory
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Initialize the router.
+	r := mux.NewRouter()
+
+	// The setup code would be very similar to the user-service's main.go
+	// Make sure to change the port number if both services will run on the same host.
+	log.Println("Starting trip service on port 5001...")
+	if err := http.ListenAndServe(":5001", r); err != nil {
+		log.Fatalf("Could not start server: %v", err)
+	}
+}
