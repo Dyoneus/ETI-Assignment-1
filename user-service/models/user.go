275b9737da93b7a8f14a4cdc92753132bd5ce1b2
Dyoneus <jiayuan999@hotmail.com>
2023-12-14 03:42:19 +0800

    Fix SQL Error 1170 & 1060

--- user-service/models/user.go
+++ user-service/models/user.go
@@ -8,14 +8,14 @@ import (
 // User represents a user in the system with a default passenger profile.
 type User struct {
 	gorm.Model
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-	FirstName string         `json:"first_name"`
-	LastName  string         `json:"last_name"`
-	Mobile    string         `json:"mobile"`
-	Email     string         `gorm:"uniqueIndex" json:"email"`
-	Password  string         `json:"password"`
-	UserType  string         `json:"user_type"`
-	Active    bool           `gorm:"default:true"`
+	//DeletedAt gorm.DeletedAt `gorm:"index"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Mobile    string `json:"mobile"`
+	Email     string `gorm:"type:varchar(255);uniqueIndex" json:"email"`
+	Password  string `json:"password"`
+	UserType  string `json:"user_type"`
+	Active    bool   `gorm:"default:true"`
 }
 
 // CarOwnerProfile represents a car owner's profile, extending the User model.
